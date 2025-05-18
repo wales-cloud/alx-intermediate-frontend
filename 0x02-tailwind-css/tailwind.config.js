79e4e2e2439a9109408a3ba6c5d764169c212146
Adewale Salami <wales.abbe@gmail.com>
2025-05-18 12:41:14 -0600

    task 2

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./1-index.html", "./src/**/*.{html,js}"],
+  content: ["./*.html", "./src/**/*.{html,js}"],
+  safelist: [
+    "grid", "grid-cols-2", "gap-2", "gap-4",
+    "bg-blue-200", "bg-blue-400", "bg-blue-500",
+    "bg-red-200", "bg-red-400", "bg-red-500",
+    "bg-green-200", "bg-yellow-200",
+    "p-2", "p-4", "text-2xl", "font-bold", "text-center"
+  ],
   theme: {
     extend: {},
   },
